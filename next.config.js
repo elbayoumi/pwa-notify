7e5671ab32760d231f6583b6405c34d21088d599
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-11-02 14:37:42 +0200

    Updated TripObserver and TripService to pass user type as an argument to TripFacadeService and TripService methods

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-
 const withPWA = require('next-pwa')({
   dest: 'public',
-  register: false,
+  register: true,
   scope: '/',
   skipWaiting: true,
 });
--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,15 @@ import '@/styles/globals.css';
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+      navigator.serviceWorker.register('/service-worker.js')
         .then(registration => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
         .catch(error => {
-          console.log('Service Worker registration failed:', error);
+          console.error('Service Worker registration failed:', error);
         });
     }
   }, []);
 
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
