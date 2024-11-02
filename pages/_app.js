d83860898bf8d023327e2c98ab30c071cc8caeed
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-11-02 15:36:08 +0200

    Updated TripObserver and TripService to pass user type as an argument to TripFacadeService and TripService methods

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,20 @@ import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
+    const enterFullscreen = () => {
+      const element = document.documentElement; // استخدام عنصر HTML الأساسي
+      if (element.requestFullscreen) {
+          element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) { // Firefox
+          element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+          element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) { // IE/Edge
+          element.msRequestFullscreen();
+      }
+  };
+  enterFullscreen();
+
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/service-worker.js')
         .then(registration => {
