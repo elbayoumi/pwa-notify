a47d9e54b240fd05a13b49ae80296a099bfeaf93
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-11-02 14:33:44 +0200

    Updated TripObserver and TripService to pass user type as an argument to TripFacadeService and TripService methods

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
-import '@/styles/globals.css'
+import { useEffect } from 'react';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(error => {
+          console.log('Service Worker registration failed:', error);
+        });
+    }
+  }, []);
+
   return (
     <>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
